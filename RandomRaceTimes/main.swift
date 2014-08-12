13cb1ea116a708052064531bc1871a0d204f58b4
Claus Guttesen <kometen@gmail.com>
2014-08-12 10:32:53 +0200

    Initial Commit

---
+++ RandomRaceTimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RandomRaceTimes
+//
+//  Created by Claus Guttesen on 12/08/14.
+//  Copyright (c) 2014 Claus Guttesen. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
